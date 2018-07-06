f4e0ed34fef1ab8be9eb255153f1e04d2ceac537
Nik <niklaus0105@gmail.com>
2018-07-05 22:30:24 -0500

    main

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import logo from '../assets/nba-logoman-word-white.svg';
 import TopNavBar from "./TopNavBar"
+import Main from "./Main"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <TopNavBar/>
-
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Main/>
       </div>
     );
   }
