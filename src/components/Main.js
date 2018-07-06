f8ea32641a3f06db7486a32b2b2f72dc30b73f74
Nik <niklaus0105@gmail.com>
2018-07-05 23:21:17 -0500

    profile

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import Profile from "./Profile";
+import nba from 'nba';
 
 export default class Main extends Component {
+    state = {
+        playerId: 201939,
+        playerInfo: {},
+    }
+    componentDidMount() {
+        const player = nba.findPlayer('Stephen Curry');
+        nba.stats.playerInfo({PlayerID: player.playerId})
+            .then(({commonPlayerInfo, playerHeadlineStats}) => {
+                const playerInfo = Object.assign({},commonPlayerInfo[0], playerHeadlineStats[0]);
+                this.setState({playerInfo});
+            });
+    }
     render() {
         return (
             <div className="main">
-                Main
+                <Profile playerInfo={this.state.playerInfo}/>
             </div>
         );
     }
