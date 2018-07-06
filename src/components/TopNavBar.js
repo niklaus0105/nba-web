36ffeb225b48a0899f439a1e285794e4ee0598e0
Nik <niklaus0105@gmail.com>
2018-07-05 22:00:08 -0500

    add logo, modify header

---
+++ src/components/TopNavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from '../assets/nba-logoman-word-white.svg';
+
+export default class TopNavBar extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        );
+    }
+}
+
